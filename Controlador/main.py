73d366f91376a9bf9098c9ad12badc7c58cc2083
Jmenabc <menajavier2002@gmail.com>
2023-01-29 12:43:42 +0100

    añadido consulta de añadir a la base de datos

--- Controlador/main.py
+++ Controlador/main.py
@@ -1,5 +1,20 @@
 from Modelo.Consultas.ConsultasPostgreSQL import ConsultasPostgreSQL
 
+#Creamos una instancia de la clase consultas para poder ejecutarlas
+
 consultas = ConsultasPostgreSQL()
 
-consultas.listarUsuarios()
+#Metemos en una lista los resultados de la lista
+
+#listaUsuarios = consultas.listarUsuarios()
+
+print('añadiendo usuario')
+print('Nombre')
+nombre = input()
+print('apellidos')
+apellidos = input()
+print('email')
+email = input()
+
+consultas.añadirUsuarios(nombre=nombre,apellidos=apellidos,email=email)
+
--- Modelo/Consultas/ConsultasPostgreSQL.py
+++ Modelo/Consultas/ConsultasPostgreSQL.py
@@ -1,6 +1,6 @@
 import Modelo.Utils
 import psycopg2
-
+from Modelo.Utils.ConexionPostgreSQL import *
 """
 Clase que contiene todas las consultas SQL
 @author Jmenabc
@@ -11,13 +11,12 @@ class ConsultasPostgreSQL:
     def listarUsuarios(self):
         try:
             print("[Modelo-Consultas-ConsultasPostgreSQL-listarUsuario]: Haciendo conexion a base de datos")
-
             con = psycopg2.connect(
-                host="localhost",
-                database="python",
-                user="postgres",
-                password="root",
-                port=5432
+                host=HOST,
+                database=DATABASE,
+                user=USER,
+                password=PASSWORD,
+                port=PORT
             )
             consulta = 'SELECT * FROM holamundo.Usuario'
             print("[Modelo-Consultas-ConsultasPostgreSQL-listarUsuario]: Abriendo conexion")
@@ -25,10 +24,29 @@ class ConsultasPostgreSQL:
             print("[Modelo-Consultas-ConsultasPostgreSQL-listarUsuario]: Ejecutamos la consulta")
             cur.execute(consulta)
             consultaEjecutada = cur.fetchall()
-            for row in consultaEjecutada:
-                print(row[0], row[1])
             print("[Modelo-Consultas-ConsultasPostgreSQL-listarUsuario]: Cerrando conexion a la base de datos")
             con.close()
+            return consultaEjecutada
 
         except Exception as error:
             print("[Modelo-Consultas-ConsultasPostgreSQL-listarUsuario]:", error)
+
+
+    def añadirUsuarios(self,nombre,apellidos,email):
+        print("[Modelo-Consultas-ConsultasPostgreSQL-añadirUsuarios]: Haciendo conexion a base de datos")
+        con = psycopg2.connect(
+            host=HOST,
+            database=DATABASE,
+            user=USER,
+            password=PASSWORD,
+            port=PORT
+        )
+
+        consulta = f'INSERT INTO holamundo.usuario (usuario_nombre, usuario_apellidos, usuario_email) VALUES ({nombre},{apellidos}, {email});'
+        print("[Modelo-Consultas-ConsultasPostgreSQL-añadirUsuarios]: Abriendo conexion")
+        cur = con.cursor()
+        print("[Modelo-Consultas-ConsultasPostgreSQL-añadirUsuarios]: Ejecutamos la consulta")
+        cur.execute(consulta)
+        print("[Modelo-Consultas-ConsultasPostgreSQL-añadirUsuarios]: Cerrando conexion a la base de datos")
+        con.close()
+
